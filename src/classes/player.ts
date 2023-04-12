7372aa76835a6dc14ce24fdc6ca30388f5bdf90b
Fabrizio Bernabei <bernabei@gmail.com>
2023-04-12 23:52:38 +0200

    animazioni player

--- src/classes/player.ts
+++ src/classes/player.ts
@@ -9,31 +9,57 @@ export class Player extends Actor {
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, 'king');
     // KEYS
-    this.keyW = this.scene.input.keyboard.addKey('W');
-    this.keyA = this.scene.input.keyboard.addKey('A');
-    this.keyS = this.scene.input.keyboard.addKey('S');
-    this.keyD = this.scene.input.keyboard.addKey('D');
+    this.keyW = this.scene.input.keyboard.addKey('UP');
+    this.keyA = this.scene.input.keyboard.addKey('LEFT');
+    this.keyS = this.scene.input.keyboard.addKey('DOWN');
+    this.keyD = this.scene.input.keyboard.addKey('RIGHT');
     // PHYSICS
     this.getBody().setSize(30, 30);
     this.getBody().setOffset(8, 0);
+
+    this.initAnimations();
   }
+
+  private initAnimations(): void {
+    this.scene.anims.create({
+      key: 'run',
+      frames: this.scene.anims.generateFrameNames('a-king', {
+        prefix: 'run-',
+        end: 7,
+      }),
+      frameRate: 8,
+    });
+    this.scene.anims.create({
+      key: 'attack',
+      frames: this.scene.anims.generateFrameNames('a-king', {
+        prefix: 'attack-',
+        end: 2,
+      }),
+      frameRate: 8,
+    });
+	}
+
   update(): void {
     this.getBody().setVelocity(0);
     if (this.keyW?.isDown) {
       this.body.velocity.y = -110;
+      !this.anims.isPlaying && this.anims.play('run', true);
     }
     if (this.keyA?.isDown) {
       this.body.velocity.x = -110;
       this.checkFlip();
       this.body.setOffset(48, 15);
+      !this.anims.isPlaying && this.anims.play('run', true);
     }
     if (this.keyS?.isDown) {
       this.body.velocity.y = 110;
+      !this.anims.isPlaying && this.anims.play('run', true);
     }
     if (this.keyD?.isDown) {
       this.body.velocity.x = 110;
       this.checkFlip();
       this.body.setOffset(15, 15);
+      !this.anims.isPlaying && this.anims.play('run', true);
     }
   }
 }
